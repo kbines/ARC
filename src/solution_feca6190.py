5d11da1ef39146108930ee3ff1c76ef2c0f8f1b6
kbines <kbines@gmail.com>
2019-12-01 20:47:31 +0000

    few typos and oopsies

--- src/solution_feca6190.py
+++ src/solution_feca6190.py
@@ -1,4 +1,4 @@
-"""NUI Galway CT5132/CT5148 Programming and Tools for AI (James McDermott)
+"""NUI Galway CT5148 Programming and Tools for AI (James McDermott)
 Task feca6190 for Assignment 3
 Student name(s): Keith Bines
 Student ID(s): 19234297
